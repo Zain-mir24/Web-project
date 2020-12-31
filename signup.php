3b13bf22ac783ce595d06af51752fe01c459177e
Zain Mir <zainnaeemk10@gmail.com>
2020-12-31 20:58:05 +0500

    working

--- signup.php
+++ signup.php
@@ -2,11 +2,7 @@
     session_start();
     $con = mysqli_connect("localhost","root","");
     mysqli_select_db($con,"webroject");
-
-    $name= $_POST["username"];
     $email = $_POST["Email"];
-    $pass=$_POST["Password"];
-
     $check = "SELECT * FROM usersignup WHERE Email='$email'";
     $result = mysqli_query($con,$check);
     $num = mysqli_num_rows($result);
@@ -14,6 +10,8 @@
         echo "username already taken";
     }
     else{
+      $name= $_POST["username"];
+      $pass=$_GET["Password"];
         $sql = "INSERT INTO usersignup(Name,Email,Password) VALUES ('$name','$email','$pass')";
         mysqli_query($con,$sql);
         echo"Registration Successfull";
